34acda6cc3ca9b3c0d9a1a0e9440caefce5f152a
Llaira <llairareyesd@gmail.com>
2020-11-23 12:05:33 -0600

    Arreglé nomenclatura

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 
 const botonIngresar=document.getElementById("ingresar")
 botonIngresar.addEventListener("click", ingresar);
+//otorga un evento click a ingresar
 
   function ingresar() {
     document.getElementById("botoningresar").style.display="none";
-    document.getElementById("pagina1").style.display="none";
-    document.getElementById("pagina2").style.display= "block";
+    document.getElementById("paginaInicio").style.display="none";
+    document.getElementById("paginaFormulario").style.display= "block";
     document.getElementById("volver").style.display="block";
+    //escondo y muestro pagina
   }
 
   
@@ -18,34 +20,44 @@ botonIngresar.addEventListener("click", ingresar);
   function volver() {
       //console.log("hola")
       document.getElementById("volver").style.display="none";
-      document.getElementById("pagina1").style.display="block";
+      document.getElementById("paginaInicio").style.display="block";
       document.getElementById("botoningresar").style.display="block";
-      document.getElementById("pagina2").style.display= "none";
+      document.getElementById("paginaFormulario").style.display= "none";
   }
          
 document.getElementById("validar").addEventListener('click', validar)
  
   function validar(){
+    //se crea la funcion 
     console.log ("hola soy validar");
-    //guardes el valor del input
     let numeroTarjeta = document.getElementById("numeroTarjeta").value;
-    console.log(numeroTarjeta);
+    //console.log(numeroTarjeta);
     let sum = 0;
-    let TarjetaInvertida = numeroTarjeta.split('').reverse().join('');
-    console.log(TarjetaInvertida);
+    //variable sumar
+    let TarjetaInvertida = numeroTarjeta.split('').reverse().join(''); //join devuelve en cadena la matriz
+    //invierto el numero de tarjeta
+    //console.log(TarjetaInvertida);
     for (let i = 0; i < TarjetaInvertida.length; i++){
+      //ciclo for para recorrer el numero de tarjeta invertido
+      //le doy inicio con el let i y el final no debe ser mayor a tarjeta invertida
      // console.log(TarjetaInvertida[i]);
         if (TarjetaInvertida.length === 16){
+          //el numero de tarjeta invertida debe ser igual a 16
           //console.log("tengo 16 digitos");
           let numero = parseInt(TarjetaInvertida.substr(i, 1));
-         // console.log(numero);
+          //entrega una cadena de numero enteros desde el incio i y otorga el valor de 1
+          //numero es la nueva variable para la cadena
+          // console.log(numero);
           if (i % 2 !==0) {
+            //el resto debe ser distinto a cero
+            //comienza la cuenta desde el inicio
               numero *= 2;
+              //multiplico x 2 
              // console.log(numero) 
             if (numero >9){
-              
+              //el numero debe ser mayor a 9 
               numero = 1 + (numero % 10);
-              //sum += numero;
+              //si el numero debe ser distinto a 10 se debe sumar 
               //console.log(numero)
 
               
@@ -56,8 +68,6 @@ document.getElementById("validar").addEventListener('click', validar)
 
       }
   
-  
-  
     }  
     //si el resultado del resto es = a cero, devolver o imprimir válida. si no es igual a cero devolver invalida
     if (sum % 10 == 0){
@@ -67,3 +77,17 @@ document.getElementById("validar").addEventListener('click', validar)
        alert ("Tarjeta Inválida")
       }
    }
+
+   
+     const maskify = (numeroTarjeta) => {
+       console.log("maskify")
+      let imprimir = "";
+          if (numeroTarjeta.length <= 4) {
+             imprimir = numeroTarjeta;
+          }
+           else {
+              //padEnd() rellena la cadena actual con una cadena dada + substring() extrae caracteres desde indiceA hasta indiceB sin incluirlo.
+            imprimir = "".padEnd(numeroTarjeta.length - 4, "#") + numeroTarjeta.substring(numeroTarjeta.length - 4, numeroTarjeta.length)
+            }
+             return imprimir
+    }
